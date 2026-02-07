b0df6369b64a08efc5632ed04ff4cb1767a2f45b
C CHANDRASEKHAR <129201593+Chandrasekhar563@users.noreply.github.com>
2026-02-07 23:39:54 +0530

    Create Cartitem.java

---
+++ Cartitem.java
@@ -0,0 +1,18 @@
+package com.example.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+    private int quantity;
+}
